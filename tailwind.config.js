5abc039a8a9a271553ad05f3af62b41e49801093
Ritish Kumar Singh <ritish@Ritishs-MacBook-Air.local>
2024-10-19 22:33:21 +0530

    Header updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -20,7 +20,7 @@ module.exports = {
   				foreground: 'hsl(var(--popover-foreground))'
   			},
   			primary: {
-  				DEFAULT: 'hsl(var(--primary))',
+  				DEFAULT: '#66B5A4',
   				foreground: 'hsl(var(--primary-foreground))'
   			},
   			secondary: {
