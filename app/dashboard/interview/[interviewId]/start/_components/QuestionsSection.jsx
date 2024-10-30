498e076717566474e9c656951d0d56d655e90d73
Ritish Kumar Singh <111806233+ritish133@users.noreply.github.com>
2024-10-30 23:24:43 +0530

    Update QuestionsSection.jsx

--- app/dashboard/interview/[interviewId]/start/_components/QuestionsSection.jsx
+++ app/dashboard/interview/[interviewId]/start/_components/QuestionsSection.jsx
@@ -19,7 +19,7 @@ function QuestionsSection({mockInterviewQuestion,activeQuestionIndex}) {
         <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5'>
             {mockInterviewQuestion&&mockInterviewQuestion.map((question,index)=>(
                 <h2 className={`p-2 bg-secondary rounded-full text-xs md:text-sm text-center cursor-pointer 
-                ${activeQuestionIndex==index&&' text-black bg-[#4ade80]'}`}>Question #{index+1}</h2>
+                ${activeQuestionIndex==index&&'text-black bg-[#4ade80]'}`}>Question #{index+1}</h2>
             ))}
 
         </div>
